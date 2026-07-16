726aae70cefe205cd40934a5fb8a2071968a115d
jacopoabramo <jacopoabramo@users.noreply.github.com>
2026-07-16 12:06:53 +0000

    deploy: edb01a9bc4a284f7fa64e2caf056874b84c4059a

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev5717+gf29386297',
+    VERSION: '0.1.dev5719+gedb01a9bc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
