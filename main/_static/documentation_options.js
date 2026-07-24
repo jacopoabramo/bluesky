1c0361c15a48a75dfb7b129d9975e69ed6bc546e
jacopoabramo <jacopoabramo@users.noreply.github.com>
2026-07-24 11:08:02 +0000

    deploy: 6f839a1a2d35ae15c9ffd291e4f7723c66ba9353

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev5719+gedb01a9bc',
+    VERSION: '0.1.dev5727+g6f839a1a2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
